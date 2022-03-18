420ac2b7d8113ca2659e1c18ab1297189ad69965
judsonpaiva <judson.paiva@hotmail.com>
2022-03-18 23:14:05 +0100

    Finalizando a App

--- app/src/main/java/com/example/appconversordemoeda/MainActivity.java
+++ app/src/main/java/com/example/appconversordemoeda/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.appconversordemoeda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt_resultado;
+    EditText edt_valor;
+    Button btn_converter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_resultado = findViewById(R.id.txt_resultado);
+        edt_valor = findViewById(R.id.edt_valor);
+        btn_converter = findViewById(R.id.btn_converter);
+
+         /*
+        btn_converter.setOnClickListener(view -> {
+            txt_resultado.setText("Oi");
+
+            double valorEuros = Double.parseDouble(edt_valor.getText().toString());
+            double valorDolares = valorEuros * 0.80;
+            txt_resultado.setText(String.valueOf(valorDolares));
+
+
+        });   */
     }
+
+    public void calcular(View view){
+        double valorEuros = Double.parseDouble(edt_valor.getText().toString());
+        double valorDolares = valorEuros * 0.80;
+        txt_resultado.setText(String.valueOf(valorDolares));
+    }
+
+
+
 }
